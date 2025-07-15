a30ea7f3f1e6c86c84fcfb272f2e596f7fba37f7
Fadisa2 <ussrysalem123@gmail.com>
2025-07-15 20:12:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-document.getElementById("bttn1").onclick = function () {
-    alert = ("تم النسخ بنجاح ")
-}
+var button = document.getElementById("btn1");
+
+button.onclick = function () {
+  alert("تم نسخ النص");
+};
+
+
+
+var button = document.getElementById("btn2");
+
+button.onclick = function () {
+  alert("تم نسخ النص");
+};
+
+
+var button = document.getElementById("btn3");
+
+button.onclick = function () {
+  alert("تم نسخ النص");
+};
+
+
